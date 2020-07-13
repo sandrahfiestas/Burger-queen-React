7a48176dfa93efb20ae16b558f128e71c74afd1c
sandymarmolejo <sandymarmolejoc@gmail.com>
2020-07-13 13:25:39 -0500

    Avance de la implementación de SPA para la page Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,19 +3,47 @@ import './Home.css';
 
 function Home() {
 
+    //const [nombre, setNombre] = React.useState();
 
+    // const soyMenu = () => {
+    //     alert("menucito")
+    // }
 
-const soyMenu = () => {
-alert("menucito");
-};
+    //const modificarTexto = (e) => {
+        //setNombre(e.target.value);
+    //}
 
-return (
-<div className="home">
-<button type="button" onclick={soyMenu}>Menú</button>
+  return (
+    <div className="home">
+        
+        <div className="container">
+        <button className="button">Menú</button>
+        {/* <button onclick={soyMenu} className="button" style={{fontSize: nombre}}>Menú</button> */}
+        <button className="button">Cocina</button>
+        </div>
 
-<button type="button">Cocina</button>
-
-</div>
-)
+     </div>
+  );
 }
+
 export default Home;
+
+
+// function Home() {
+
+
+
+//     const soyMenu = () => {
+//     alert("menucito");
+//     };
+    
+//     return (
+//     <div className="home">
+//     <button type="button" onclick={soyMenu}>Menú</button>
+    
+//     <button type="button">Cocina</button>
+    
+//     </div>
+//     )
+//     }
+//     export default Home;
