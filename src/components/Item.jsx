42015bf6a7130b55f34b0f399e70047a1cc947c3
sandrahfiestas <shfiestas@gmail.com>
2020-07-23 09:03:43 -0500

    componente producto del menú

---
+++ src/components/Item.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Item.scss';
+
+function Item(props) {
+    console.log(props);
+  return (
+    <div className="breakfast-item">
+      <img src="" className="breakfast-product" alt="coffee" />
+      {/* <p>Café americano</p> */}
+      {props.product}
+      <div>
+        <button>
+          <img src="" className="icons-more-less" alt="icon_less" />
+        </button>
+        S/5.00
+        <img src="" className="icons-more-less" alt="icon_more" />
+      </div>
+    </div>
+  );
+}
+
+export default Item;
