8b5e9198f9e6bed72ecd56bde72cb3aef4dd7500
sandrahfiestas <shfiestas@gmail.com>
2020-07-23 09:00:03 -0500

    cambiando nombre de carperta components a pages

---
+++ src/pages/Kitchen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Kitchen.css';
+
+function Kitchen() {
+    return (
+        <div className="kitchen">
+            
+            <div className="container">
+            <p>Cocinando</p>
+            </div>
+    
+         </div>
+      );
+}
+
+export default Kitchen;
