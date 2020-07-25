1a053c4a9e262d033b16bedcd26e6796b74a1a70
sandrahfiestas <shfiestas@gmail.com>
2020-07-25 12:14:17 -0500

    eliminando comentarios adicionales

--- src/pages/Kitchen.jsx
+++ src/pages/Kitchen.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import './Kitchen.css';
+import './Kitchen.scss';
 
 function Kitchen() {
-    return (
-        <div className="kitchen">
-            
-            <div className="container">
-            <p>Cocinando</p>
-            </div>
-    
-         </div>
-      );
+  return (
+    <div className="kitchen">
+      <div className="container">
+        <p>Cocinando</p>
+      </div>
+    </div>
+  );
 }
 
 export default Kitchen;
