96df1b3aa0fedb33c073ec9095ac29b8df603508
sandrahfiestas <shfiestas@gmail.com>
2020-07-26 15:53:44 -0500

    modicando link de src a imagen de firebase

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,6 +3,10 @@ import firebase from '../firebase/firebase';
 import './Item.scss';
 import iconMore from '../images/icon_more.png';
 import iconLess from '../images/icon_less.png';
+// import coffee from '../images/coffee.jpg';
+// import latte from '../images/latte.jpg';
+// import juice from '../images/juice.jpg';
+// import sandwich from '../images/sandwich.jpg';
 
 function Item() {
   // useState
@@ -11,7 +15,7 @@ function Item() {
 
   // useEffect
   useEffect(() => {
-    const unsubscribe = firebase.firestore().collection('menu').onSnapshot((snap) => {
+    const unsubscribe = firebase.firestore().collection('menu').orderBy('id', 'asc').onSnapshot((snap) => {
       const array = [];
       snap.forEach((doc) => {
         array.push(doc.data());
@@ -25,23 +29,28 @@ function Item() {
     <>
       {product.map((r) => (
         <div className="breakfast-item" key={r.id}>
-          <figure><img className="breakfast-product" src="https://source.unsplash.com/_1x_x8Vtg2w/800x600" alt="imagen" /></figure>
-          {r.Product}
+          <figure><img className="breakfast-product" src={r.Link} alt="imagen" /></figure>
+          {/* {coffee}
+          {latte}
+          {juice}
+          {sandwich} */}
+          <b>{r.Product}</b>
           <br />
-          {r.Price}
-
           <div>
-            <button onClick={() => setCount(count > 0 ? count - 1 : count)}>
+            <button className="btn-counters  espacito" onClick={() => setCount(count > 0 ? count - 1 : count)}>
               <img src={iconLess} className="icons-more-less" alt="icon_less" />
             </button>
-
-            <button onClick={() => setCount(count + 1)}>
+            <b>
+              S/.
+              {r.Price}
+            </b>
+            <button className="btn-counters espacio" onClick={() => setCount(count + 1)}>
               <img src={iconMore} className="icons-more-less" alt="icon_more" />
             </button>
-            {count}
+            {/* {count} */}
           </div>
-
         </div>
+
       ))}
     </>
   );
