97c7eaa5da39dd21fc9d2aa15c423d174039570b
sandrahfiestas <shfiestas@gmail.com>
2020-07-27 12:28:07 -0500

    Mostrando datos de .json mediante Hooks

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,56 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import firebase from '../firebase/firebase';
 import './Item.scss';
-import iconMore from '../images/icon_more.png';
-import iconLess from '../images/icon_less.png';
-// import coffee from '../images/coffee.jpg';
-// import latte from '../images/latte.jpg';
-// import juice from '../images/juice.jpg';
-// import sandwich from '../images/sandwich.jpg';
 
 function Item() {
   // useState
-  const [product, setProduct] = useState([]);
-  const [count, setCount] = useState(0);
+  // Al renderizar genera un estado (menu)
+  // La función (setMenu) actualiza el estado del componente...
+  // aceptando un nuevo valor de estado.
+  const [menu, setMenu] = useState([0]);
 
   // useEffect
+  // Lee los datos alojados en menuList.json
   useEffect(() => {
-    const unsubscribe = firebase.firestore().collection('menu').orderBy('id', 'asc').onSnapshot((snap) => {
-      const array = [];
-      snap.forEach((doc) => {
-        array.push(doc.data());
+    fetch('../menuList.json')
+      .then((response) => response.json())
+      .then((datos) => {
+        setMenu(datos);
       });
-      setProduct(array);
-    });
-    return unsubscribe;
   }, []);
 
   return (
     <>
-      {product.map((r) => (
-        <div className="breakfast-item" key={r.id}>
-          <figure><img className="breakfast-product" src={r.Link} alt="imagen" /></figure>
-          {/* {coffee}
-          {latte}
-          {juice}
-          {sandwich} */}
-          <b>{r.Product}</b>
-          <br />
-          <div>
-            <button className="btn-counters  espacito" onClick={() => setCount(count > 0 ? count - 1 : count)}>
-              <img src={iconLess} className="icons-more-less" alt="icon_less" />
-            </button>
-            <b>
-              S/.
-              {r.Price}
-            </b>
-            <button className="btn-counters espacio" onClick={() => setCount(count + 1)}>
-              <img src={iconMore} className="icons-more-less" alt="icon_more" />
-            </button>
-            {/* {count} */}
-          </div>
+      {menu.map((product) => (
+        <div className="breakfast-item" key={product.id}>
+          <figure><img className="breakfast-product" src={`${process.env.PUBLIC_URL}/imagesProduct/${product.image}`} alt="imagen" /></figure>
+          {product.name}
+          <p>
+            S./
+            {product.price}
+          </p>
+          <button className="btn-add">AGREGAR</button>
         </div>
-
       ))}
     </>
   );
