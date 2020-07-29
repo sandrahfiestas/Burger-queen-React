0c047e3d21e3d43a1de1ebe375ca00a44212bacf
sandrahfiestas <shfiestas@gmail.com>
2020-07-28 23:22:54 -0500

    hooks useState y useEffect para clock

--- src/pages/Breakfast.jsx
+++ src/pages/Breakfast.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Breakfast.scss';
 import logo from '../images/logo.png';
 import menuBurger from '../images/menu_burger.png';
 import Item from '../components/Item';
+import iconMore from '../images/icon_more.png';
+import iconLess from '../images/icon_less.png';
+import remove from '../images/remove.png';
 
 function Breakfast() {
+  const [clock, setClock] = useState(new Date().toLocaleString([], { hour: '2-digit', hour12: true, minute: '2-digit' }));
+  useEffect(() => {
+    // setInterval -> ejetuta una función en intervalos
+    const interval = setInterval(() => {
+      // toLocaleString([locale [, options]]);
+      setClock(new Date().toLocaleString([], { hour: '2-digit', hour12: true, minute: '2-digit' }));
+    }, 1000);
+    // clearInterval -> borra intervalos
+    return () => clearInterval(interval);
+  }, []);
+
   return (
 
     <div>
@@ -40,31 +54,48 @@ function Breakfast() {
 
         <div className="breakfast-ticket-btn">
           <div className="breakfast-ticket">
-            <div className="item-client">
-              <p>
-                Ciente :
-                <input type="text" placeholder="Nombre de cliente" className="validaty" pattern="([a-zA-ZÁÉÍÓÚñáéíóúÑ]{1,30}\\s*)+" />
-              </p>
+            <div className="ticket-header">
+              <div className="ticket-client">
+                <p>
+                  Ciente :
+                  <input type="text" placeholder="Nombre de cliente" className="validaty input-client" pattern="([a-zA-ZÁÉÍÓÚñáéíóúÑ]{1,30}\\s*)+" />
+                </p>
+              </div>
+              <div className="ticket-time">
+                <p>
+                  Hora de atención:
+                  {clock}
+                </p>
+                <p>
+                  Número de Mesa :
+                  <input type="text" placeholder="N° mesa" className="validaty input-table" pattern="" />
+                </p>
+              </div>
             </div>
-            <div className="item-time">
-              <p>Hora de atención:</p>
-              <p>Número de Mesa:</p>
-            </div>
-            <div className="item-product">
-              <p>PRODUCTO</p>
-            </div>
-            <div className="item-unit">
-              <p>UNID.</p>
-            </div>
-            <div className="item-price">
-              <p>PRECIO</p>
-            </div>
-            <div className="item-total">
-              <p>TOTAL DE PEDIDO</p>
-            </div>
-            <div className="item-total-price">
-              <p>S/.</p>
-            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col" />
+                  <th scope="col" />
+                  <th scope="col">Cant.</th>
+                  <th scope="col">Producto</th>
+                  <th scope="col">Precio</th>
+                  <th scope="col">Total</th>
+                  <th scope="col">Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="col"><img src={iconMore} alt="" /></th>
+                  <th scope="col"><img src={iconLess} alt="" /></th>
+                  <th scope="col">10</th>
+                  <th scope="col">Sandwich de jamón y queso</th>
+                  <th scope="col">10</th>
+                  <th scope="col">100</th>
+                  <th scope="col"><img src={remove} alt="" /></th>
+                </tr>
+              </tbody>
+            </table>
           </div>
           <div className="breakfast-btns">
             <button className="btn-accept">CONFIRMAR</button>
