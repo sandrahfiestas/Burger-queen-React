7185a16e5e2d00986a2d412e3acf8bc1986e6bd6
sandrahfiestas <shfiestas@gmail.com>
2020-08-03 13:01:34 -0500

    enviando datos de inputs-form a firebase

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
-// import firebase from '../firebase/firebase';
+import firebase from '../firebase/firebase';
 import './styleComponents/Summary.scss';
+import Clock from './Clock';
 import iconMore from '../images/icon_more.png';
 import iconLess from '../images/icon_less.png';
 import iconDelete from '../images/icon_delete.png';
 
 const Summary = (props) => {
-  const [name, setName] = React.useState('');
-  const [table, setTable] = React.useState('');
+
+  const initialStateValues = {
+    table: '',
+    name: '',
+    state: 'Por preparar',
+  };
+
   const [, setResult] = React.useState(props.summary);
+  const [values, setValues] = React.useState(initialStateValues);
 
-  const nameClient = (e) => {
-    setName(e.target.value);
+  // Manejando cambio de entrada
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
   };
 
-  const numberTable = (e) => {
-    setTable(e.target.value);
+  const handleSubmit = (e) => {
+    // cancelando su comportamiento por defecto
+    e.preventDefault();
+    if (!values.name.trim() || !values.table.trim()) {
+      console.log('Campo(s) vacio(s). Verifique');
+    // return;
+    } else {
+      firebase.firestore().collection('pedidos').add({
+        name: values.name,
+        table: values.table,
+      })
+        .then(() => {
+          console.log('Pedido guardado');
+        })
+        .catch((error) => {
+          console.log('Error al guardar peido:', error);
+        });
+
+      console.log(values);
+      setValues({ ...initialStateValues });
+    }
   };
+
+  const cancelOrder = (e) => {
+    e.preventDefault();
+    setValues({ ...initialStateValues });
+  };
+
 
   const btnMore = (item) => {
     const array = props.summary;
@@ -43,61 +77,63 @@ const Summary = (props) => {
   };
 
   return (
-    <div className="">
-      <div className="breakfast-ticket">
+    <form onSubmit={handleSubmit}>
 
-        <div className="ticket-header">
-          <div className="">
-            <p>
-              Ciente :
-              <input type="text" onChange={nameClient} placeholder="Nombre de cliente" value={name} className="validaty input-client" pattern="([a-zA-ZÁÉÍÓÚñáéíóúÑ]{1,30}\\s*)+" />
-            </p>
-          </div>
-          <div className="">
-            <p>
-              Hora de atención:
-              {/* {clock} */}
-            </p>
-            <p>
-              Número de Mesa :
-              <input type="text" onChange={numberTable} placeholder="N° mesa" value={table} className="validaty input-table" pattern="" />
-            </p>
-          </div>
-        </div>
+      <div className="">
+        <div className="breakfast-ticket">
 
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th scope="col" />
-                <th scope="col" />
-                <th scope="col">Cant.</th>
-                <th scope="col">Producto</th>
-                <th scope="col">P/U</th>
-                <th scope="col">Total</th>
-                <th scope="col">Eliminar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                props.summary.map((item, i) => (
-                  <tr key={i} id={item.idProduct}>
-                    <th scope="col"><img src={iconMore} onClick={(e) => btnMore(item)} alt="" /></th>
-                    <th scope="col"><img src={iconLess} onClick={(e) => btnLess(item)} alt="" /></th>
-                    <th scope="col"><p className="">{item.countProduct}</p></th>
-                    <th scope="col">{item.nameProduct}</th>
-                    <th scope="col">
-                      S/
-                      {item.priceProduct}
-                    </th>
-                    <th scope="col">
-                      S/
-                      {item.priceProduct * item.countProduct}
-                    </th>
-                    <th scope="col"><img src={iconDelete} id={i} onClick={deleteItem} alt="" /></th>
-                  </tr>
-                ))
-              }
+          <div className="ticket-header">
+            <div className="">
+              <p>
+                Ciente :
+                <input type="text" name="name" onChange={handleInputChange} value={values.name} placeholder="Nombre de cliente" className="validaty input-client" />
+              </p>
+            </div>
+            <div className="">
+              <p>
+                Hora de atención:
+                <Clock />
+              </p>
+              <p>
+                Número de Mesa :
+                <input type="text" name="table" onChange={handleInputChange} value={values.table} placeholder="N° mesa" className="validaty input-table" />
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col" />
+                  <th scope="col" />
+                  <th scope="col">Cant.</th>
+                  <th scope="col">Producto</th>
+                  <th scope="col">P/U</th>
+                  <th scope="col">Total</th>
+                  <th scope="col">Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  props.summary.map((item, i) => (
+                    <tr key={item.idProduct}>
+                      <th scope="col"><img src={iconMore} onClick={(e) => btnMore(item)} alt="" /></th>
+                      <th scope="col"><img src={iconLess} onClick={(e) => btnLess(item)} alt="" /></th>
+                      <th scope="col"><p className="">{item.countProduct}</p></th>
+                      <th scope="col">{item.nameProduct}</th>
+                      <th scope="col">
+                        S/
+                        {item.priceProduct}
+                      </th>
+                      <th scope="col">
+                        S/
+                        {item.priceProduct * item.countProduct}
+                      </th>
+                      <th scope="col"><img src={iconDelete} id={i} onClick={deleteItem} alt="" /></th>
+                    </tr>
+                  ))
+                }
                 <tr>
                   <th colSpan={7}>
                     <p>
@@ -108,16 +144,19 @@ const Summary = (props) => {
                 </tr>
               </tbody>
             </table>
+          </div>
+
+        </div>
+
+        <div className="breakfast-btns">
+          <button className="btn-accept">CONFIRMAR</button>
+          <button className="btn-cancel" onClick={cancelOrder}>CANCELAR</button>
         </div>
 
       </div>
 
-      <div className="breakfast-btns">
-        <button className="btn-accept">CONFIRMAR</button>
-        <button className="btn-cancel">CANCELAR</button>
-      </div>
+    </form>
 
-    </div>
   );
 };
 
